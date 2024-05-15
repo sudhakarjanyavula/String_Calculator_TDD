246809ccd3b1ee6dd11bed7cf86bbe5d55ed93b7
sudhakar <sudhakarjanyavula@gmail.com>
2024-05-15 21:53:14 +0530

    [commit for handle an any amount of numbers]

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -8,5 +8,13 @@ class Calculator
           return nums.reduce(0, :+)
         end
     end
+    def multiple_numbers(numbers)
+      if numbers.empty?
+        return 0
+      else
+        nums = numbers.split(",").map(&:to_i)
+        return nums.reduce(0, :+)
+      end
+    end
 end
   
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,7 +7,7 @@ describe Calculator do
   end
 
   context "Given two numbers" do 
-    it "returns 0 for an empty string" do
+      it "returns 0 for an empty string" do
         sum = @calc.add("")
         expect(sum).to eql(0)
       end
@@ -20,6 +20,12 @@ describe Calculator do
       it "returns the sum of two numbers" do
         sum = @calc.add("2,3")
         expect(sum).to eql(5)
+      end
+    end
+    context "Given multiple numbers" do
+      it "returns the sum of multiple numbers" do
+        sum = @calc.multiple_numbers("1,2,3,4,5")
+        expect(sum).to eql(15)
       end
     end
 end
